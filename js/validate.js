bd3ec3b7da53cc86b44be89d3e28941e23eb69b5
Jonathansartorib <jonathansartorib@hotmail.com>
2022-07-28 19:14:29 -0300

    Atualização e correção

--- js/validate.js
+++ js/validate.js
@@ -1,1 +1,18 @@
 //Seu JavaScript de validação aqui
+
+var btnSubmit = document.querySelector(".formcontato__botao");
+var nome = document.querySelector("#nome");
+var email = document.querySelector("#email");
+var assunto = document.querySelector("#assunto");
+var mensagem = document.querySelector("#mensagem");
+
+btnSubmit.addEventListener("click", function(evento) {
+    evento.preventDefault();
+
+    console.log(nome.value);
+    console.log(email.value);
+    console.log(assunto.value);
+    console.log(mensagem.value);
+
+    document.querySelector('form').reset();
+})
